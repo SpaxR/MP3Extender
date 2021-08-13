77bfc2b6aba9cb7df2913ca82988adac2658ec89
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-08-13 17:51:11 +0200

    Added Tests for App.xaml.cs

---
+++ Tests.Unit/AppTests.cs
@@ -0,0 +1,23 @@
+using UI;
+using Xunit;
+
+namespace Tests.Unit
+{
+	public class AppTests : TestBase<App>
+	{
+		/// <inheritdoc />
+		protected override App CreateSUT() => new();
+
+		[Fact]
+		public void GivenCreated_WhenCreated_ThenHasServices()
+		{
+			Assert.NotNull(SUT.Services);
+		}
+
+		[Fact]
+		public void GivenCreated_WhenCreated_ThenCurrentIsCurrentInstance()
+		{
+			Assert.Equal(SUT, App.Current);
+		}
+	}
+}
