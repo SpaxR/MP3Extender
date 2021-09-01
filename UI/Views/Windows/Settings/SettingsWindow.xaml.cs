d8d6822ba9c8ff1487e61cff79d37d604e13bfe2
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-09-01 16:00:53 +0200

    Added Basic Config and SettingsWindow

--- UI/Views/Windows/Settings/SettingsWindow.xaml.cs
+++ UI/Views/Windows/Settings/SettingsWindow.xaml.cs
@@ -1,10 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+
 namespace UI.Settings
 {
+	[ExcludeFromCodeCoverage]
 	public partial class SettingsWindow
 	{
-		public SettingsWindow()
-		{
-			InitializeComponent();
-		}
+		public SettingsWindow() => InitializeComponent();
+
+		private void CloseWindow(object sender, RoutedEventArgs e) => Close();
 	}
 }
