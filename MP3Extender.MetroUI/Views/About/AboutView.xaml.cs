f21e0045deee54ca5927665d1f63fd8800c6057a
SpaxR <55739719+SpaxR@users.noreply.github.com>
2021-09-13 21:23:25 +0200

    Added MahApps.Metro with basic Views and ViewModels

---
+++ MP3Extender.MetroUI/Views/About/AboutView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace MP3Extender.MetroUI.Views.About
+{
+	public partial class AboutView : UserControl
+	{
+		public AboutView()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ MP3Extender.MetroUI/Views/Editor/EditorView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace MP3Extender.MetroUI.Views.Editor
+{
+	public partial class EditorView : UserControl
+	{
+		public EditorView()
+		{
+			InitializeComponent();
+		}
+	}
+}
